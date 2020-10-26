cdd84ab3357de742a34877eec12efcc8ce20fc02
Hridu <hridu@stanford.edu>
2020-10-26 16:45:42 -0700

    Section 6

--- scripts/controllers/tets.py
+++ scripts/controllers/tets.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env  python
-# encoding: utf-8
+
 import numpy as np
 from utils import wrapToPi
 import math
-import  rospy
-from std_msgs.msg import Float64
+#import  rospy
+#from std_msgs.msg import Float64
 
 # command zero velocities once we are this close to the goal
 RHO_THRES = 0.05
@@ -14,23 +13,18 @@ DELTA_THRES = 0.1
 class PoseController:
     """ Pose stabilization controller """
     def __init__(self, k1, k2, k3, V_max=0.5, om_max=1):
-        #rospy.init_node('turtlebot_controller', anonymous=True)
-        #rospy.init_node('controller_talker' , anonymous=True)
-        #rate = rospy.Rate(10) # 10 Hz
-        #rate.sleep()
-
-        self.pub_alpha = rospy.Publisher('/controller/alpha',Float64,queue_size=10)
-        self.pub_delta =rospy.Publisher('/controller/delta',Float64,queue_size=10)
-        self.pub_rho =rospy.Publisher('/controller/rho',Float64,queue_size=10)
-
         self.k1 = k1
         self.k2 = k2
         self.k3 = k3
 
         self.V_max = V_max
         self.om_max = om_max
+#        self.pub_alpha = rospy.Publisher('/controller/alpha' , Float64 , queue_size =10)
+#        self.pub_delta = rospy.Publisher('/controller/delta' , Float64 , queue_size =10)
+#        self.pub_rho = rospy.Publisher('/controller/rho' , Float64 , queue_size =10)
 
-        
+#        rospy.init_node('controller_talker' , anonymous=True)
+
 
     def load_goal(self, x_g, y_g, th_g):
         """ Loads in a new goal position """
@@ -68,14 +62,13 @@ class PoseController:
         V=self.k1*rho*np.cos(alpha)
         om=self.k2*alpha+self.k1*np.sin(alpha)*np.cos(alpha)/alpha*(alpha+self.k3*delta)
 
-        
-        #publish
-        self.pub_alpha.publish((Float64)(alpha))
-        self.pub_delta.publish((Float64)(delta))
-        self.pub_rho.publish((Float64)(rho))
-        #rate = rospy.Rate(10) # 10 Hz
-        #rate.sleep()
 
+        #publish
+        # self.pub_alpha.publish(alpha)
+        # self.pub_delta.publish(delta)
+        # self.pub_rho.publish(rho)
+
+     
         
         ########## Code ends here ##########
 
